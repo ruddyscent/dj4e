97855230b69b015e8740ce197b3dd97e67877cfd
Kyungwon Chun <ruddyscent@gmail.com>
2022-06-15 01:20:58 +0900

    Complete week 1 of Django Features and Libraries

---
+++ mysite/hello/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def sessfun(request):
+    num_visits = request.session.get('num_visits', 0) + 1
+    request.session['num_visits'] = num_visits
+    if num_visits > 5: del(request.session['num_visits'])
+    resp = HttpResponse('view count=' + str(num_visits))
+    resp.set_cookie('dj4e_cookie', '98ba58ed', max_age=1000)
+    return resp
+
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,35 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+import os
+from django.contrib import admin
+from django.urls import path, include, re_path
+# from django.conf.urls import url
+from django.views.static import serve
+from django.views.generic import TemplateView
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.join(BASE_DIR, 'site')
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
+    path('polls/', include('polls.urls')),
+    path('hello/', include('hello.urls')),
+    path('admin/', admin.site.urls),
+    re_path(r'^site/(?P<path>.*)$', serve,
+    {'document_root': SITE_ROOT, 'show_indexes': True},
+    name='site_path'),
+]
+
