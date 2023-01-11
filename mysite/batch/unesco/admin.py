17467718127ac2fde0e6c76add5644ca8637caf6
Kyungwon Chun <ruddyscent@gmail.com>
2023-01-11 16:41:51 +0900

    Complete 'Design a Data Model'

--- mysite/batch/unesco/admin.py
+++ mysite/batch/unesco/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from unesco.models import Category, State, Iso, Region, Site
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(State)
+admin.site.register(Iso)
+admin.site.register(Region)
+admin.site.register(Site)
